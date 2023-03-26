a03d3ce24b2783f1d12a9586f5baf6a4754e3f26
Felipe Costa <felipecosta776@outlook.com>
2023-03-26 17:33:49 -0300

    adds footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,51 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className=" bg-white shadow m-4 dark:bg-gray-800">
+      <div className="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2040{" "}
+          <Link to="/" className="hover:underline">
+            Filmpedia
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <a
+              href="#"
+              target="_blank"
+              className="mr-4 hover:underline md:mr-6 "
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              target="_blank"
+              className="mr-4 hover:underline md:mr-6"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              target="_blank"
+              className="mr-4 hover:underline md:mr-6"
+            >
+              YouTube
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" className="hover:underline">
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
