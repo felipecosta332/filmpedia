81aecaf66bf3adda66eb6cb94bba222e0c04cb50
Felipe Costa <felipecosta776@outlook.com>
2023-04-05 18:12:47 -0300

    api fetch film list

--- src/pages/FilmList.js
+++ src/pages/FilmList.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import { Card } from "../components/Card";
 
 export const FilmList = () => {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    async function fetchFilms() {
+      const APIKey = "476f7930f8c9d29bf755d06bb62e6dae";
+      const APIRequest =
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=";
+      const response = await fetch(`${APIRequest}${APIKey}`);
+      const data = await response.json();
+      setFilms(data.results);
+    }
+    fetchFilms();
+  }, []);
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
-          <Card />
+          {films.map((film) => (
+            <Card key={film.id} film={film} />
+          ))}
         </div>
       </section>
     </main>
